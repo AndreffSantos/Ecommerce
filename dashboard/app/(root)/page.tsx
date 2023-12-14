1cdd4cd94f24849e0eaa29030fe8c42d2a856fe8
Andre Santos <andrefelipe_santos@outlook.com>
2023-12-14 13:17:52 -0300

    Install Button from shadcn.

--- dashboard/app/(root)/page.tsx
+++ dashboard/app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='container'>
-      <h1 className='text-center'>Admin Dashboard</h1>
+        <div className='flex justify-center items-center'>
+            <h1>Admin Dashboard</h1>
+            <Button className='m-2'>Click aqui.</Button>
+        </div>
     </main>
   )
 }
