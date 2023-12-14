7967a0dd084538b91c78ddb104bd72d006156153
Andre Santos <andrefelipe_santos@outlook.com>
2023-12-14 13:00:42 -0300

    Change Layout and delete page structure.

---
+++ dashboard/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='container'>
+      <h1 className='text-center'>Admin Dashboard</h1>
+    </main>
+  )
+}
